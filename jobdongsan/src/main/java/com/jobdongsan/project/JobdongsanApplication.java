321b55044335812467d4b1c2a18a6bcf91a96616
Shuroop2 <kingdho3o@gmail.com>
2022-08-16 10:10:28 +0900

    update application file

--- jobdongsan/src/main/java/com/jobdongsan/project/JobdongsanApplication.java
+++ jobdongsan/src/main/java/com/jobdongsan/project/JobdongsanApplication.java
@@ -1,9 +1,13 @@
 package com.jobdongsan.project;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.jobdongsan.project"})
+@MapperScan(basePackages = {"com.jobdongsan.project"})
 public class JobdongsanApplication {
 
 	public static void main(String[] args) {
